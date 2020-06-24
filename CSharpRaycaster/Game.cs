fb4ab0f3d25a1a884a89b8998bc3907225ca3525
Irishmun <steefiscool70@gmail.com>
2020-06-24 16:47:56 +0200

    added projects
    added C and C# project alongside needed libraries

---
+++ CSharpRaycaster/Game.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenTK;
+using OpenTK.Graphics;
+
+namespace CSharpRaycaster
+{
+    class Game : GameWindow
+    {
+        public Game(int width, int height, string title) : base(width, height, GraphicsMode.Default, title)
+        {
+
+        }
+
+        public void main()
+        {
+
+        }
+    }
+}
