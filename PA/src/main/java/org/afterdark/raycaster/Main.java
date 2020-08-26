e711f4e3c6075b26dac7766910a8ae43dcd40049
Irishmun <steefiscool70@gmail.com>
2020-08-26 15:00:49 +0200

    added PA to master

---
+++ PA/src/main/java/org/afterdark/raycaster/Main.java
@@ -0,0 +1,18 @@
+package org.afterdark.raycaster;
+
+import org.afterdark.raycaster.World.*;
+
+public class Main
+{
+    private Map map = new Map();
+
+    public static void main(String[] args)
+    {
+        new Main().Run();
+    }
+
+    void Run()
+    {
+        System.out.println("[Main]: Run method reached");
+    }
+}
